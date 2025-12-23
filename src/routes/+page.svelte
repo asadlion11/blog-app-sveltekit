f8db3cb8eb0bced197c34f30f1aa2b1c7286ec77
asadlion11 <mabdiaziz416@gmail.com>
2025-12-23 14:31:48 +0300

    Better Auth setup for server side  & client side

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-lg font-bold">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit" class="text-blue-500">svelte.dev/docs/kit</a> to read the documentation</p>
